b8d4290e4d78e5b097862c90372dcf675a843483
dinda672 <152958257+dinda672@users.noreply.github.com>
2024-05-03 23:28:20 +0700

    koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,26 @@
+<?php
+
+class DatabaseConnection
+{
+
+    protected static ?PDO $connection = null;
+
+    public function __construct()
+    {
+        echo "<br>New Database Instance is Created!<br>";
+    }
+
+    public static function connect(): PDO
+    {
+        try {
+            if(!self::$connection){
+                $pdo = new PDO('mysql:host=localhost;dbname=perpustakaan;port:3306', 'root', '');
+                self::$connection = $pdo;
+            }
+            return self::$connection;
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+        echo "<br>";
+    }
+}
